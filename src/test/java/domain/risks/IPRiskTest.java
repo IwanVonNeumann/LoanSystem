4d90230eb5a2f00314bd491bc5ee7578fbcfd7cb
Iwan <grabageheap@inbox.lv>
2014-09-21 22:47:11 +0300

    Still doing Risk Tests

--- src/test/java/domain/risks/IPRiskTest.java
+++ src/test/java/domain/risks/IPRiskTest.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 import static junit.framework.Assert.assertFalse;
+import static junit.framework.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 /**
@@ -45,6 +46,29 @@ public class IPRiskTest {
         testLoan.setIpAddress(ip);
 
         assertFalse(ipRisk.isHigh(testLoan));
+    }
+
+    @Test
+    public void test3sameIPs() {
+
+        String ip = "192.168.1.1";
+
+        Loan loan1 = new Loan();
+        loan1.setIpAddress(ip);
+
+        Loan loan2 = new Loan();
+        loan2.setIpAddress(ip);
+
+        Loan loan3 = new Loan();
+        loan3.setIpAddress(ip);
+
+        when(loanDAO.getListForTheLastDay(ip)).thenReturn(
+                new ArrayList<Loan>(Arrays.asList(loan1, loan2, loan3)));
+
+        Loan testLoan = new Loan();
+        testLoan.setIpAddress(ip);
+
+        assertTrue(ipRisk.isHigh(testLoan));
     }
 
 }
